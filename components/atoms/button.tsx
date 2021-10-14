d74d0fd477c4d9301ad12b053eb8aefec099d3d0
Rizky Darmawan <darmawanrizky43@gmail.com>
2021-10-14 14:18:29 +0700

    Add component button

---
+++ components/atoms/button.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ButtonItemProps {
+  title: String;
+  icon: string;
+  colorButton: String;
+  href: String;
+}
+export default function Button(props: ButtonItemProps) {
+  // Get proprty props
+  const { title, icon, colorButton, href = "#" } = props;
+  return (
+    <div>
+      <Link href={href}>
+        <a
+          className={`bg-${colorButton}-500 hover:bg-${colorButton}-700 transition-all duration-150 px-2 py-2 rounded text-white font-semibold inline-flex items-center mb-2 lg:mb-0`}
+        >
+          <div className="mr-1 w-5 h-5 relative">
+            <Image
+              src={`/icon/button/${icon}.svg`}
+              layout="fill"
+              objectFit="contain"
+              alt="pluc-circle-icon"
+            />
+          </div>
+          {title}
+        </a>
+      </Link>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/atoms/button";
 import Sidebar from "../components/organisms/Sidebar";
 
 export default function index() {
@@ -10,26 +11,33 @@ export default function index() {
         </div>
 
         {/* Category content */}
-        <div className="py-5 px-5 w-full lg:w-10/12 inline-flex">
-          {/* div 1 */}
-          <div className="py-5 px-5 w-full">
-            <div className="border-2 rouded   bg-white">
-              {/* title*/}
-              <div className="flex p-2 select-none">
-                <div className="heading text-gray-800 w-full pl-3 font-semibold my-auto border-b">
-                  Home
-                </div>
+        <div className="py-5 px-5 w-full lg:w-10/12">
+          {/* div 1 content */}
+          <div className="mb-5">
+            <Button
+              title="Create"
+              icon="plus-circle"
+              colorButton="green"
+              href="tambah-data-revenue"
+            />
+          </div>
+          {/* div 2 */}
+          <div className="border-2 rouded   bg-white">
+            {/* title*/}
+            <div className="flex p-2 select-none">
+              <div className="heading text-gray-800 w-full pl-3 font-semibold my-auto border-b">
+                Home
               </div>
-              {/* Show Home Content */}
-              <div className="flex p-2 select-one">
-                <div className="heading text-gray-800 w-full pl-3 font-semibold my-auto px-2">
-                  <div className="mt-3">
-                    <div className="w-full mb-5">
-                      <div className="text-gray-700 font-normal bg-blue-200 rounded-lg px-2 py-2">
-                        Selamat datang{" "}
-                        <span className="font-semibold">Rizky</span>, Kamu login
-                        sebagai super Admin
-                      </div>
+            </div>
+            {/* Show Home Content */}
+            <div className="flex p-2 select-one">
+              <div className="heading text-gray-800 w-full pl-3 font-semibold my-auto px-2">
+                <div className="mt-3">
+                  <div className="w-full mb-5">
+                    <div className="text-gray-700 font-normal bg-blue-200 rounded-lg px-2 py-2">
+                      Selamat datang{" "}
+                      <span className="font-semibold">Rizky</span>, Kamu login
+                      sebagai super Admin
                     </div>
                   </div>
                 </div>
